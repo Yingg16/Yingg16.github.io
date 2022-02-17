3000471e6fbbd1d9747dceaf98fb677ebfce2d14
Yingg16 <64537604+Yingg16@users.noreply.github.com>
2022-02-17 15:24:57 +0800

    final version 0.1

--- script.js
+++ script.js
@@ -5,11 +5,40 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         } 
+        if(this.scroll >500){
+            $('.scrollup').addClass("show");
+        }else{
+            $('.scrollup').removeClass("show");
+        }
     });
 
+    // slide-up script
+    $('.scrollup').click(function(){
+        $('html').animate({scrollTop: 0});
+        $('html').css("scrollBehavior", "auto");
+    });
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
     //toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     }); 
+
+    //owl carousel script
+    $(".carousel").owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive:{
+          0:{
+              items: 1,
+            },                           
+        }
+    });
+
 });
